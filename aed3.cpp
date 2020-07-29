fe57056ec17dee27c871361ec757e417b8df5667
Andre <dezin-oliveira@hotmail.com>
2020-07-29 20:47:43 -0300

    Teste

---
+++ aed3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    string txt;
+
+    cout << "Digite seu nome: ";
+    cin >> txt;
+    cout << "Bem-vindo " << txt << "!";
+
+    return 0;
+}
